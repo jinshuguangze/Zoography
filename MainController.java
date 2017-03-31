bd89bcf6d5687e662cfe116f25929c977621565d
jinshuguangze <408256363@qq.com>
2017-03-31 17:38:33 +0800

    Zoography
    为计算机设计大赛所制作的一个软件,旨在科普常见动物以及其分类,习性,并呼吁人们保护珍稀动物.

---
+++ MainController.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.collections.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class MainController {
+
+    @FXML
+    private Pane Main_Left_Top_Pane;
+
+    @FXML
+    private BorderPane Main_BorderPane;
+
+    @FXML
+    private Pane Main_Left_Pane;
+
+    @FXML
+    private Pane Main_Left_Middle_Pane;
+
+    @FXML
+    private Pane Main_Left_Bottom_Pane;
+
+    @FXML
+    private ListView<String> Main_Left_Bottom_ListView;
+    
+
+}
