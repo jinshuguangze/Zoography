c88e97dae0b3831c129d49d3b73a01d83a1dbb66
jinshuguangze <408256363@qq.com>
2017-03-31 17:52:03 +0800

    Zoography
    为计算机设计大赛所制作的一个软件,旨在科普常见动物以及其分类,习性,并呼吁人们保护珍稀动物.

---
+++ Initialization.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.collections.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+public class Initialization {
+    
+    protected Initialization() {
+    	/*lookup
+    	ObservableList<String> Main_Left_Bottom_ListView_Item =FXCollections.observableArrayList ("分层查看", "搜索模式", "随机页面", "个性设置");
+    	Main_Left_Bottom_ListView.setItems(Main_Left_Bottom_ListView_Item);*/
+	}
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+package application;
+	
+import javafx.application.*;
+import javafx.collections.*;
+import javafx.fxml.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+			
+			ListView Main_Left_Bottom_ListView=(ListView)root.lookup("#Main_Left_Bottom_ListView");
+			ObservableList<String> Main_Left_Bottom_ListView_Item =FXCollections.observableArrayList ("分层查看", "搜索模式", "随机页面", "个性设置");
+			Main_Left_Bottom_ListView.setItems(Main_Left_Bottom_ListView_Item);
+			
+			Scene scene = new Scene(root,1200,800);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());	
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
