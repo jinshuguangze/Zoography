79ee519bd793ad7c621045d2bb6b7f788e466d0c
jinshuguangze <408256363@qq.com>
2017-04-02 18:41:57 +0800

    Zoography

--- Main.java
+++ Main.java
@@ -13,21 +13,31 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			//创建根节点
 			Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
 			
-			Scene scene = new Scene(root,1200,800);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());			
+			//读取屏幕大小
+			double screenWidth=java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+			double screenHeight = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;			
+			double defaultWidth=0.75*screenWidth;
+			double defaultHeight=0.75*screenHeight;
 			
+			//添加样式
+			Scene scene = new Scene(root,defaultWidth,defaultHeight);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			//primaryStage.initStyle(StageStyle.TRANSPARENT);
+						
+			//左侧ListView加标签
 			ListView Main_BorderPane_VBox_ListView=(ListView)root.lookup("#Main_BorderPane_VBox_ListView");			
 			ObservableList<String> Main_BorderPane_VBox_ListView_Item =FXCollections.observableArrayList ("分层查看", "搜索模式", "随机页面", "个性设置");	
 			Main_BorderPane_VBox_ListView.setItems(Main_BorderPane_VBox_ListView_Item);			
 								
-			primaryStage.heightProperty().addListener((ov,t,t1)->{
-				
-			});
+			//primaryStage.heightProperty().addListener((ov,t,t1)->{});
 			
-			primaryStage.setScene(scene);
+			//显示
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
